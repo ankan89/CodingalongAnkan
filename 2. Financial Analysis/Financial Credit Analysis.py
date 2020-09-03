c12cc4d26a72122a7678c986cb825f6b4a237209
ankan89 <60543101+ankan89@users.noreply.github.com>
2020-09-03 06:05:20 +0530

    Revolving Credit Jupiter Notebook

--- 2. Financial Analysis/Financial Credit Analysis.py
+++ 2. Financial Analysis/Financial Credit Analysis.py
@@ -2,5 +2,9 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-dataset = pd.read_csv()
+dataset = pd.read_csv("/Users/Ankan/Desktop/DataScience/GitHub/New-Tariff-Rule-/2. Financial Analysis/Report.csv",encoding='latin1')
+
+
+print(dataset.head())
+
 
